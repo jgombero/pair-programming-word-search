335cd780f07d3651fd163683dd203877c682c552
Joshua Gomberoff <joshuagomberoff@gmail.com>
2020-06-04 02:47:26 +0000

    function returns whether or not a word is in a nested array

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,17 @@
 const wordSearch = (letters, word) => { 
     const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+    const regex = RegExp(word, "g");
+   
+    const result = [];
+    for (let i = 0; i < letters[0].length; i++) {
+        result[i] = [];
+        for (let j = 0; j < letters.length; j++) {
+            result[i].push(letters[j][i]);
+        }
     }
-}
+    const realResult = result.map(ls => ls.join(''));
+    
+    return regex.test(horizontalJoin) || regex.test(realResult);
+};
 
-module.exports = wordSearch
+module.exports = wordSearch;
